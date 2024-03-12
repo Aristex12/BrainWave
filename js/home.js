67f3129accf94292416d3dce31c23bb0cda1b31b
Aristex12 <aristex@hotmail.com>
2024-03-12 04:42:38 +0100

    Se han añadido animaciones en la home

--- js/home.js
+++ js/home.js
@@ -1,31 +1,64 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtener todos los elementos con la clase "tarjeta_servicio"
-    const serviciosDivs = document.querySelectorAll('.tarjeta_servicio');
-
-    // Asignar un evento de clic a cada div
-    serviciosDivs.forEach(div => {
-        div.addEventListener('click', function () {
-            // Obtener el id del div clicado
-            const divId = this.id;
-
-            // Redirigir a la página correspondiente
-            switch (divId) {
-                case 'psicologo':
-                    window.location.href = 'psicologos.php';
-                    break;
-                case 'workshops':
-                    window.location.href = 'workshops.php';
-                    break;
-                case 'articulos':
-                    window.location.href = 'articulos.php';
-                    break;
-                case 'libros':
-                    window.location.href = 'libros.php';
-                    break;
-                default:
-                    // Manejar cualquier otro caso si es necesario
-                    break;
-            }
-        });
+$(document).ready(function () {
+    // Manejar clic en tarjetas de servicios
+    $('.tarjeta_servicio').click(function () {
+      const divId = this.id;
+  
+      switch (divId) {
+        case 'psicologo':
+          window.location.href = 'psicologos.php';
+          break;
+        case 'workshops':
+          window.location.href = 'workshops.php';
+          break;
+        case 'articulos':
+          window.location.href = 'articulos.php';
+          break;
+        case 'libros':
+          window.location.href = 'libros.php';
+          break;
+        default:
+          // Manejar cualquier otro caso si es necesario
+          break;
+      }
     });
-});
+  
+    // Animación de porcentajes al desplazarse
+    const elementosPorcentaje = $(".circulo .inner #number");
+    const section5Element = $(".section_5");
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            iniciarAnimacionesPorcentaje();
+            observer.unobserve(section5Element[0]);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+  
+    observer.observe(section5Element[0]);
+  
+    function iniciarAnimacionesPorcentaje() {
+      elementosPorcentaje.each(function () {
+        const porcentajeFinal = parseInt($(this).text(), 10);
+        const duracion = 2000; // Duración de la animación en milisegundos
+        const incremento = porcentajeFinal / (duracion / 16.7); // Aproximadamente 60 FPS
+        let porcentajeActual = 0;
+  
+        const actualizarPorcentaje = () => {
+          if (porcentajeActual < porcentajeFinal) {
+            porcentajeActual += incremento;
+            $(this).text(Math.ceil(porcentajeActual) + "%");
+            setTimeout(actualizarPorcentaje, 16.7);
+          } else {
+            $(this).text(porcentajeFinal + "%");
+          }
+        };
+  
+        actualizarPorcentaje();
+      });
+    }
+  });
+  
